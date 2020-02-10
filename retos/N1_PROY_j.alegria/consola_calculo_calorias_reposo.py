5f69696dbf25f6d4fca3637083a3ab3d80bce5d5
Juan Sebastián Alegría Zúñiga <juanszalegria@gmail.com>
2020-02-10 01:33:53 -0500

    n1

--- retos/N1_PROY_j.alegria/consola_calculo_calorias_reposo.py
+++ retos/N1_PROY_j.alegria/consola_calculo_calorias_reposo.py
@@ -11,7 +11,7 @@ import calculadora_indices as calc
 def ejecutar_calcular_calorias_en_reposo() -> None:
     peso = float(input("Ingrese el peso de la persona en kilogramos: "))
     altura = float(input("Ingrese la altura de la persona en centimetros: "))
-    edad = int(input("Ingrese la edad de la persona: "))
+    edad = int(input("Ingrese la edad de la persona en años: "))
     valor_genero = float(input("Ingrese el valor -161 si es mujer, o 5 en caso de ser hombre: "))
     tmb = calc.calcular_calorias_en_reposo(peso, altura, edad, valor_genero)
     print("\nLa persona quema " + str(tmb) + " cal en reposo")
--- retos/N1_PROY_j.alegria/consola_calculo_porcentaje_grasa.py
+++ retos/N1_PROY_j.alegria/consola_calculo_porcentaje_grasa.py
@@ -12,7 +12,7 @@ def ejecutar_calcular_porcentaje_grasa() -> None:
     peso = float(input("Ingrese el peso de la persona en kilogramos: "))
     altura = float(input("Ingrese la altura de la persona en metros: "))
     imc = calc.calcular_IMC(peso, altura)
-    edad = int(input("Ingrese la edad de la persona: "))
+    edad = int(input("Ingrese la edad de la persona en años: "))
     valor_genero = float(input("Ingrese el valor 0 si es mujer, o 10.8 en caso de ser hombre: "))
     pgc = calc.calcular_porcentaje_grasa(imc, edad, valor_genero)
     print("\nSu porcentaje de grasa corporal es " + str(pgc) + "%")
